6fda1a34679ecf5beca91021b59551f7c54c9ba0
unknown <houj@oxy.edu>
2019-09-24 22:13:00 -0700

    Created new components for collecting feedback

--- part2/src/index.js
+++ part2/src/index.js
@@ -2,18 +2,51 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-  return (
-    <div>
-      code here
-    </div>
-  )
+    return (
+        <div>
+            <Titles text="give feedback" />
+            <Rating handleClick={() => setGood(good + 1)} rate="good" />
+            <Rating handleClick={() => setNeutral(neutral + 1)} rate="neutral" />
+            <Rating handleClick={() => setBad(bad + 1)} rate="bad" />
+            <Titles text="statistics" />
+            <Display text="good" value={good}/>
+            <Display text="neutral" value={neutral}/>
+            <Display text="bad" value={bad}/>
+        </div>
+    )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+const Titles = (props) => {
+    return (
+        <div>
+            <h1>{props.text}</h1>
+        </div>
+    )
+}
+
+const Rating = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleClick}>
+                {props.rate}
+            </button>
+        </div>
+    )
+}
+
+const Display=(props)=>{
+    return(
+        <div>
+            {props.text} {props.value}
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root')
 )
