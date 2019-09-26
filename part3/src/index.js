495d92d36b4b9646dc48dfcacc100c7802f3b49e
Jonathan Hou <houj@oxy.edu>
2019-09-25 21:33:52 -0700

    Attempted to add and keep track of votes

--- part3/src/index.js
+++ part3/src/index.js
@@ -4,21 +4,46 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+
+  const copy = Array.apply(null, new Array(10)).map(Number.prototype.valueOf,0);
+
+  
   let random = Math.floor(Math.random() * props.anecdotes.length)
-    console.log(random)
+  
+  console.log(random)
+  console.log("random" , copy[random])
+  console.log(copy)
+  
   return (
     <div>
       {props.anecdotes[selected]}
-
+      <Display copy = {copy[random]}/>
+      <Vote handleClick2={()=>copy[random]+=1}/> 
       <Button handleClick={()=>setSelected(random)}/>
     </div>
   )
 }
 
+const Display=(props)=>{
+    return(
+        <div>
+            has {props.copy} votes
+        </div>
+    )
+}
+
 const Button=(props)=>{
     return(
         <div> 
-            <button onClick={props.handleClick}>next anectode</button>
+            <button onClick={props.handleClick2}>next anectode</button>
+        </div>
+    )
+}
+
+const Vote=(props)=>{
+    return(
+        <div>
+            <button onClick={props.handleClick}>vote</button>
         </div>
     )
 }
