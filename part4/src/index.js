2b25e26d79c8d3c832de15cbf620f67897b7527b
Jonathan Hou <houj@oxy.edu>
2019-09-27 15:23:14 -0700

    First map method

--- part4/src/index.js
+++ part4/src/index.js
@@ -25,13 +25,13 @@ const notes = [
 const App = (props) => {
   const { notes } = props
 
+  const rows = ()=> notes.map(note=> <li key={note.id}>{note.content}</li>)
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {rows()}
       </ul>
     </div>
   )
