98889388e0159221a8072a942407039a90480d3d
unknown <houj@oxy.edu>
2019-09-30 22:10:00 -0700

    Prevent user from adding duplicates

--- part5/src/index.js
+++ part5/src/index.js
@@ -2,64 +2,61 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'First Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([
+        { name: 'First Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
 
-  const handleNameChange=(event)=>{
-    console.log(event.target.value)
-    setNewName(event.target.value)
-    
-}
+    const handleNameChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
 
-const notification=(props)=>{
-    return(
+    }
+
+    const notification = (props) => {
+        return (
+            <div>
+                alert()
+        </div>
+        )
+    }
+
+
+    const addNote = (event) => {
+        event.preventDefault()
+        if (!(persons.map(p => p.name).includes(newName))) {
+            const personObject = {
+                name: newName
+            }
+            setPersons(persons.concat(personObject))
+        }
+        else {
+            alert(`${newName} is already added to phonebook`)
+        }
+
+        setNewName('')
+
+    }
+
+    return (
+
         <div>
-            alert()
+            <h2>Phonebook</h2>
+            <form onSubmit={addNote}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange} />
+                    <button type="submit" onClick={notification}>add</button> </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(p => <li key={p.name}>{p.name}</li>)}
         </div>
     )
 }
 
 
-const addNote=(event)=>{
-    event.preventDefault()
-    console.log(persons.map(p=>p.name).includes(newName))
-    console.log(persons.map(p=>p.name))
-    if(!(persons.map(p=>p.name).includes(newName)))
-    {
-        const personObject={
-            name: newName
-        }
-        setPersons(persons.concat(personObject))
-    }
-    else{
-        alert(`${newName} is already added to phonebook`)
-    }
-
-    setNewName('')
-
-}
-
-  return (
-      
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addNote}>
-          <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-          <button type="submit" onClick={notification}>add</button> </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map(p=><li key={p.name}>{p.name}</li>)}
-    </div>
-  )
-}
-
-
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById('root')
-  )
+)
 
