eb88b77bd3f5bfb1e93459d72f6219bf334ee63d
unknown <houj@oxy.edu>
2019-10-02 19:35:24 -0700

    Restrict search to 10 results or less

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,42 +1,51 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Input=({country, setCountry})=>{
-  
+const Input = ({ country, setCountry }) => {
 
-  const handleSetCountry=(event)=>{
-        console.log(event.target.value)
-        setCountry(event.target.value)
+
+  const handleSetCountry = (event) => {
+    console.log(event.target.value)
+    setCountry(event.target.value)
   }
 
-  return(
+  return (
     <div>
-      find countries <input value={country} onChange={handleSetCountry}/>
+      find countries <input value={country} onChange={handleSetCountry} />
     </div>
   )
 }
 
 
-const Result=({country, person})=>{
+const Result = ({ country, person }) => {
 
-if (country !== "") {
-        console.log("Yes")
-        return (
-            <div>
-                {person.map(p => (p.name.toLowerCase()).includes(country.toLowerCase()) ? <li key={p.name}>{p.name}</li> : <div key={p.name}></div>)}
-            </div>
-        )
+  if (country !== "") {
+    console.log("Yes")
+    var count = 0;
+    (person.map(p => (p.name.toLowerCase()).includes(country.toLowerCase()) ? count++ : null))
+    if (count <= 10) {
+      return (
+        person.map(p => (p.name.toLowerCase()).includes(country.toLowerCase()) ? <li key={p.name}>{p.name}</li> : <div key={p.name}></div>)
+      )
     } else {
-        return (
-            <div>
-                {person.map(p => <li key={p.name}>{p.name} </li>)}
+      return (
+        <div>
+          search is too broad.
             </div>
-        )
+      )
     }
+
+  } else {
+    return (
+      <div>
+        Enter search term
+            </div>
+    )
+  }
 }
 
-const App=()=>{
-  const [person, setPerson] = useState([]); 
+const App = () => {
+  const [person, setPerson] = useState([]);
   const [country, setCountry] = useState('')
 
   useEffect(() => {
@@ -44,19 +53,19 @@ const App=()=>{
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-          console.log('promise fulfilled')
-          setPerson(response.data)   
+        console.log('promise fulfilled')
+        setPerson(response.data)
       })
   }, [])
 
-  console.log('render', person.length,person)
+  console.log('render', person.length, person)
 
-    return(
-      <div>
-        <Input country={country} setCountry={setCountry}/>
-        <Result country={country} person={person}/>
-      </div>
-    )
+  return (
+    <div>
+      <Input country={country} setCountry={setCountry} />
+      <Result country={country} person={person} />
+    </div>
+  )
 }
 
 
